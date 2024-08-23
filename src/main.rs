454e91284f22e39cbcfd9ee13ee57094dd3c853b
Wataru Ishikawa <wataruby@gmail.com>
2024-08-23 22:20:13 +0900

    add ut

--- src/main.rs
+++ src/main.rs
@@ -44,13 +44,53 @@ async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
     (StatusCode::CREATED, Json(user))
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
 struct CreateUser {
     username: String,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Debug, Deserialize, PartialEq, Eq)]
 struct User {
     id: u64,
     username: String,
+}
+
+#[cfg(test)]
+mod test {
+    use std::usize;
+
+    use super::*;
+    use axum::body::{to_bytes, Body};
+    use axum::http::{header, Method, Request};
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        let bytes = to_bytes(res.into_body(), usize::MAX).await.unwrap();
+        let body = String::from_utf8(bytes.to_vec()).unwrap();
+        assert_eq!(body, "Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn should_return_user_data() {
+        let req = Request::builder()
+            .method(Method::POST)
+            .uri("/users")
+            .header(header::CONTENT_TYPE, "application/json")
+            .body(Body::from(r#"{"username":"田中太郎"}"#))
+            .unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        let bytes = to_bytes(res.into_body(), usize::MAX).await.unwrap();
+        let body = String::from_utf8(bytes.to_vec()).unwrap();
+        let user = serde_json::from_str::<User>(&body).unwrap();
+        assert_eq!(
+            user,
+            User {
+                id: 1337,
+                username: "田中太郎".to_string()
+            }
+        );
+    }
 }

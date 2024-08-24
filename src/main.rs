3d1fd6e50f99fb277cefd44b0db208447e10dbbd
Wataru Ishikawa <wataruby@gmail.com>
2024-08-24 14:51:48 +0900

    remove

--- src/main.rs
+++ src/main.rs
@@ -115,23 +115,12 @@ fn create_app<T: TodoRepository>(repository: T) -> Router {
     Router::new()
         .route("/", get(root))
         .route("/todos", post(create_todo::<T>))
-        .route("/users", post(create_user))
         .with_state(Arc::new(repository))
 }
 
 async fn root() -> &'static str {
     tracing::debug!("Handling GET request to /");
     "Hello, World!"
-}
-
-async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
-    tracing::debug!("Handling POST request to /users");
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-    tracing::info!("Created user: {:?}", user);
-    (StatusCode::CREATED, Json(user))
 }
 
 async fn create_todo<T: TodoRepository>(
@@ -142,23 +131,12 @@ async fn create_todo<T: TodoRepository>(
     (StatusCode::CREATED, Json(todo))
 }
 
-#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
-struct CreateUser {
-    username: String,
-}
-
-#[derive(Serialize, Debug, Deserialize, PartialEq, Eq)]
-struct User {
-    id: u64,
-    username: String,
-}
-
 #[cfg(test)]
 mod test {
 
     use super::*;
     use axum::body::{to_bytes, Body};
-    use axum::http::{header, Method, Request};
+    use axum::http::Request;
     use tower::ServiceExt;
 
     #[tokio::test]
@@ -169,27 +147,5 @@ mod test {
         let bytes = to_bytes(res.into_body(), usize::MAX).await.unwrap();
         let body = String::from_utf8(bytes.to_vec()).unwrap();
         assert_eq!(body, "Hello, World!");
-    }
-
-    #[tokio::test]
-    async fn should_return_user_data() {
-        let repository = TodoRepositoryForMemory::new();
-        let req = Request::builder()
-            .method(Method::POST)
-            .uri("/users")
-            .header(header::CONTENT_TYPE, "application/json")
-            .body(Body::from(r#"{"username":"田中太郎"}"#))
-            .unwrap();
-        let res = create_app(repository).oneshot(req).await.unwrap();
-        let bytes = to_bytes(res.into_body(), usize::MAX).await.unwrap();
-        let body = String::from_utf8(bytes.to_vec()).unwrap();
-        let user = serde_json::from_str::<User>(&body).unwrap();
-        assert_eq!(
-            user,
-            User {
-                id: 1337,
-                username: "田中太郎".to_string()
-            }
-        );
     }
 }

3cd6ea468512501fcc37bc15c9b42be63cbc8f09
Wataru Ishikawa <wataruby@gmail.com>
2024-08-30 23:03:55 +0900

    implement handler

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,8 +1,12 @@
-use axum::{extract::State, response::IntoResponse, Json};
+use axum::{
+    extract::{Path, State},
+    response::IntoResponse,
+    Json,
+};
 use hyper::StatusCode;
 use std::sync::Arc;
 
-use crate::repositories::{CreateTodo, TodoRepository};
+use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
 
 pub async fn create_todo<T: TodoRepository>(
     State(repository): State<Arc<T>>,
@@ -10,4 +14,38 @@ pub async fn create_todo<T: TodoRepository>(
 ) -> impl IntoResponse {
     let todo = repository.create(payload);
     (StatusCode::CREATED, Json(todo))
+}
+
+pub async fn find_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    State(repository): State<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository.find(id).ok_or(StatusCode::NOT_FOUND)?;
+    Ok((StatusCode::OK, Json(todo)))
+}
+
+pub async fn all_todo<T: TodoRepository>(State(repository): State<Arc<T>>) -> impl IntoResponse {
+    let todos = repository.all();
+    (StatusCode::OK, Json(todos))
+}
+
+pub async fn update_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    State(repository): State<Arc<T>>,
+    Json(payload): Json<UpdateTodo>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository
+        .update(id, payload)
+        .or(Err(StatusCode::NOT_FOUND))?;
+    Ok((StatusCode::OK, Json(todo)))
+}
+
+pub async fn delete_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    State(repository): State<Arc<T>>,
+) -> StatusCode {
+    repository
+        .delete(id)
+        .map(|_| StatusCode::NO_CONTENT)
+        .unwrap_or(StatusCode::NOT_FOUND)
 }
